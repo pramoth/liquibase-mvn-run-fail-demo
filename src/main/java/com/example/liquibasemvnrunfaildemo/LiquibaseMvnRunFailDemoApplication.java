637ca74cf85bc6d9e8728f7656059d343e89b76c
Pramoth <pramoth@geniustree.co.th>
2021-01-22 01:15:47 +0700

    demo liquibase mvn spring-boot;run fail

---
+++ src/main/java/com/example/liquibasemvnrunfaildemo/LiquibaseMvnRunFailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasemvnrunfaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseMvnRunFailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseMvnRunFailDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/changelog/sql/0000_xxx.sql
@@ -0,0 +1,1 @@
+create table XXX(id number);
